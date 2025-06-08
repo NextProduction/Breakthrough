d9dbcae71399b12af038bb4de749c1ada7791be3
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2025-06-08 22:18:51 +0330

    Debug

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['icanquit.app', 'github.com', 'img.shields.io'],
+    domains: ['nextproduction.dev', 'github.com', 'img.shields.io'],
     formats: ['image/avif', 'image/webp'],
     unoptimized: true,
   },
@@ -23,7 +23,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  output : "standalone"
+  output : "export"
 }
 
 // Bundle analyzer plugin - only active when ANALYZE=true
